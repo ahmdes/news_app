87587dd4691860ff5b93929e87253a120e5f27b0
ahmed_mostafa <ahmed.mostafa.elshekh@gmail.com>
2025-09-13 10:29:32 +0300

    my message

--- lib/views/catogry_list_view.dart
+++ lib/views/catogry_list_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../models/image_and_text_model.dart';
 import '../widgets/category_card.dart';
 
-class CategoryListView extends StatelessWidget {
+class CategoryListView extends StatefulWidget {
   const CategoryListView({
     super.key,
     required this.imageAndText,
@@ -12,15 +12,25 @@ class CategoryListView extends StatelessWidget {
   final List<ImageAndText> imageAndText;
 
   @override
+  State<CategoryListView> createState() => _CategoryListViewState();
+}
+
+class _CategoryListViewState extends State<CategoryListView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 130,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
 
-        itemCount:imageAndText.length ,
+        itemCount:widget.imageAndText.length ,
         itemBuilder: (context,int index){
-          return CategoryCard(imageAndText: imageAndText[index]);
+          return CategoryCard(imageAndText: widget.imageAndText[index]);
         },
       ),
     );
